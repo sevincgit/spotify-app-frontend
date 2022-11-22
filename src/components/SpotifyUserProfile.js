ccd8670b6ec597aad46937bb167796c8078ff113
sevincgit <sevincucar91@gmail.com>
2022-11-22 01:06:36 +0100

    moving myprofile and saveuserdata functions in useEffect

--- src/components/SpotifyUserProfile.js
+++ src/components/SpotifyUserProfile.js
@@ -9,51 +9,66 @@ const SpotifyUserProfile = (props) => {
   const [spotifyUserID, setSpotifyUserID] = useState('');
   const [spotifyUserImageUrl, setSpotifyUserImageUrl] = useState('');
 
-  const myProfile = async () => {
-    try {
-      const { data } = await axios.get(USERPROFILE_ENDPOINT, {
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer ' + props.token,
-        },
-      });
-      console.log(USERPROFILE_ENDPOINT);
-      console.log('token received', props.token);
-      console.log('data', data);
-      setSpotifyProfileName(data.display_name);
-      setSpotifyUserID(data.id);
-      setSpotifyUserImageUrl(data.images[0].url);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  myProfile();
-
-  const saveUserData = async () => {
-    const currentUserData = {
-      userid: spotifyUserID,
-      username: spotifyProfileName,
-    };
-    let usersPath = `${process.env.REACT_APP_SPOTIFYAPP_API}/users`;
-    try {
-      let response = await fetch(usersPath, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-type': 'application/json',
-        },
-        body: JSON.stringify(currentUserData),
-      });
-      console.log(response);
-      if (response.status === 201) {
-        console.log('new user saved');
+  useEffect(() => {
+    const myProfile = async () => {
+      if (!props.token) {
+        return;
       }
-    } catch (error) {
-      console.log(error);
-    }
-  };
+      console.log('getting spotify profile');
+      try {
+        const { data } = await axios.get(USERPROFILE_ENDPOINT, {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + props.token,
+          },
+        });
+        console.log(USERPROFILE_ENDPOINT);
+        console.log('token received', props.token);
+        console.log('data', data);
+        setSpotifyProfileName(data.display_name);
+        setSpotifyUserID(data.id);
+        setSpotifyUserImageUrl(data.images[0].url);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    myProfile();
+  }, [props.token]);
+
+  useEffect(() => {
+    const saveUserData = async () => {
+      if (!spotifyUserID || !spotifyProfileName) {
+        return;
+      }
+      console.log('saving user to db');
+      const currentUserData = {
+        id: spotifyUserID,
+        username: spotifyProfileName,
+      };
+      let usersPath = `${process.env.REACT_APP_SPOTIFYAPP_API}/users`;
+      try {
+        let response = await fetch(usersPath, {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify(currentUserData),
+        });
+        console.log('response', response);
+        let responseData = await response.json();
+        console.log('responseData', responseData);
+        if (response.status === 201) {
+          console.log('new user saved');
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    saveUserData();
+  }, [spotifyProfileName, spotifyUserID]);
 
   return (
     <div>
