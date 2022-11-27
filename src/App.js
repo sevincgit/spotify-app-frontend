e32ba7f9dbd4ab0e438f3c867c8e9fa766a61681
sevincgit <sevincucar91@gmail.com>
2022-11-27 21:08:33 +0100

    removing mui theme from app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { useState } from 'react';
 import Header from './components/Header';
 import MyPlaylists from './components/MyPlaylists';
 import Playlist from './components/Playlist';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
@@ -14,46 +13,25 @@ function App() {
   const [token, setToken] = useState('');
   const [spotifyUserID, setSpotifyUserID] = useState('');
 
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: 'rgb(30, 215, 96)',
-      },
-      secondary: {
-        main: 'rgb(30, 215, 96)',
-      },
-    },
-    underline: {
-      '&&&:before': {
-        borderBottom: 'none',
-      },
-      '&&:after': {
-        borderBottom: 'none',
-      },
-    },
-  });
-
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <div className='App'>
-          {loggedIn ? (
-            <>
-              <Header setLoggedIn={setLoggedIn} token={token} setToken={setToken} spotifyUserID={spotifyUserID} setSpotifyUserID={setSpotifyUserID} />
-              <Routes>
-                <Route path='/' element={<CreatePlaylist token={token} spotifyUserID={spotifyUserID} />} />
-                <Route path='/my-playlists' element={<MyPlaylists spotifyUserID={spotifyUserID} token={token} />} />
-                <Route path='/my-playlists/:id/:playlistName' element={<Playlist spotifyUserID={spotifyUserID} token={token} />} />
-                <Route path='/about' element={<div>About Component</div>} />
-                <Route path='*' element={<div>Pag not found</div>} />
-              </Routes>
-            </>
-          ) : (
-            <SpotifyLogin setLoggedIn={setLoggedIn} token={token} setToken={setToken} />
-          )}
-        </div>
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <div className='App'>
+        {loggedIn ? (
+          <>
+            <Header setLoggedIn={setLoggedIn} token={token} setToken={setToken} spotifyUserID={spotifyUserID} setSpotifyUserID={setSpotifyUserID} />
+            <Routes>
+              <Route path='/' element={<CreatePlaylist token={token} spotifyUserID={spotifyUserID} />} />
+              <Route path='/my-playlists' element={<MyPlaylists spotifyUserID={spotifyUserID} token={token} />} />
+              <Route path='/my-playlists/:id/:playlistName' element={<Playlist spotifyUserID={spotifyUserID} token={token} />} />
+              <Route path='/about' element={<div>About Component</div>} />
+              <Route path='*' element={<div>Pag not found</div>} />
+            </Routes>
+          </>
+        ) : (
+          <SpotifyLogin setLoggedIn={setLoggedIn} token={token} setToken={setToken} />
+        )}
+      </div>
+    </BrowserRouter>
   );
 }
 
