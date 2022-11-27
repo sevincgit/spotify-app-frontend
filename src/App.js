d22070180e57d82ccefe5718050b3e24b3d56c29
sevincgit <sevincucar91@gmail.com>
2022-11-27 23:09:23 +0100

    mui theme added again

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { useState } from 'react';
 import Header from './components/Header';
 import MyPlaylists from './components/MyPlaylists';
 import Playlist from './components/Playlist';
+import About from './components/About';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
@@ -13,25 +15,38 @@ function App() {
   const [token, setToken] = useState('');
   const [spotifyUserID, setSpotifyUserID] = useState('');
 
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: 'rgb(30, 215, 96)',
+      },
+      secondary: {
+        main: '#f44336',
+      },
+    },
+  });
+
   return (
-    <BrowserRouter>
-      <div className='App'>
-        {loggedIn ? (
-          <>
-            <Header setLoggedIn={setLoggedIn} token={token} setToken={setToken} spotifyUserID={spotifyUserID} setSpotifyUserID={setSpotifyUserID} />
-            <Routes>
-              <Route path='/' element={<CreatePlaylist token={token} spotifyUserID={spotifyUserID} />} />
-              <Route path='/my-playlists' element={<MyPlaylists spotifyUserID={spotifyUserID} token={token} />} />
-              <Route path='/my-playlists/:id/:playlistName' element={<Playlist spotifyUserID={spotifyUserID} token={token} />} />
-              <Route path='/about' element={<div>About Component</div>} />
-              <Route path='*' element={<div>Pag not found</div>} />
-            </Routes>
-          </>
-        ) : (
-          <SpotifyLogin setLoggedIn={setLoggedIn} token={token} setToken={setToken} />
-        )}
-      </div>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <div className='App'>
+          {loggedIn ? (
+            <>
+              <Header setLoggedIn={setLoggedIn} token={token} setToken={setToken} spotifyUserID={spotifyUserID} setSpotifyUserID={setSpotifyUserID} />
+              <Routes>
+                <Route path='/' element={<CreatePlaylist token={token} spotifyUserID={spotifyUserID} />} />
+                <Route path='/my-playlists' element={<MyPlaylists spotifyUserID={spotifyUserID} token={token} />} />
+                <Route path='/my-playlists/:id/:playlistName' element={<Playlist spotifyUserID={spotifyUserID} token={token} />} />
+                <Route path='/about' element={<About />} />
+                <Route path='*' element={<div>Pag not found</div>} />
+              </Routes>
+            </>
+          ) : (
+            <SpotifyLogin setLoggedIn={setLoggedIn} token={token} setToken={setToken} />
+          )}
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
